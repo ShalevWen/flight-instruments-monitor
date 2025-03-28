bab18dfae69ae669265ea2b551ec1ebc296865df
ShalevWen <shalevwen@gmail.com>
2025-03-28 18:29:35 +0300

    Rewrote the HIS gauge

--- client/src/components/instruments/VisualHisInstrument.tsx
+++ client/src/components/instruments/VisualHisInstrument.tsx
@@ -1,4 +1,19 @@
+// using trigonometry to translate from the center
+function getTranslation(angle: number): string {
+  return `translateY(${-Math.cos((angle * Math.PI) / 180) * 42}%) translateX(${
+    -Math.sin((angle * Math.PI) / 180) * 42
+  }%)`;
+}
+
 export default function VisualHisInstrument({ value = 0 }: { value: number }) {
+  function AngleIndicator({ angle }: { angle: number }) {
+    return (
+      <div className="his-gauge-number" style={{ transform: getTranslation(value - angle) }}>
+        {angle}
+      </div>
+    );
+  }
+
   return (
     <div className="his-gauge-container">
       <div className="his-gauge">
@@ -7,25 +22,10 @@ export default function VisualHisInstrument({ value = 0 }: { value: number }) {
       <div className="his-gauge-arrow">
         <div className="his-gauge-arrow-tip">&#9650;</div>
       </div>
-      <div className="his-gauge-numbers-container">
-        <table style={{ transform: `rotate(${-value}deg)` }}>
-          <tr>
-            <td />
-            <td className="his-gauge-number-0">0</td>
-            <td />
-          </tr>
-          <tr>
-            <td className="his-gauge-number-270">270</td>
-            <td />
-            <td className="his-gauge-number-90">90</td>
-          </tr>
-          <tr>
-            <td />
-            <td className="his-gauge-number-180">180</td>
-            <td />
-          </tr>
-        </table>
-      </div>
+      <AngleIndicator angle={0} />
+      <AngleIndicator angle={90} />
+      <AngleIndicator angle={180} />
+      <AngleIndicator angle={270} />
     </div>
   );
 }
